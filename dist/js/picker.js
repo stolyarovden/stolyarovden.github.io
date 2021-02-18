afe63f8ee953f4439e56fd4bb7be3a37189aeec4
stolyarovden <stolyarovden@gmail.com>
2021-02-18 17:59:46 +0300

    Add files via upload

---
+++ dist/js/picker.js
@@ -0,0 +1,81 @@
+var mod0 = "Привет! 👋 Меня зовут Денис, я занимаюсь кадровыми, просветительскими и digital-проектами в STEM-образовании. Работаю в лучшем молодом университете России — <a href='https://student.skoltech.ru' target='_blank'>Сколтехе</a>, где руковожу привлечением и отбором студентов, курирую центр развития карьеры, студенческие сервисы и программы развития сообщества студентов и выпускников. Мои контакты <a href='#me' onclick='pickMe(5)'>здесь</a>.";
+var mod1 = "<a href='https://www.instagram.com/p/CLblDuGDR0X/'  target='_blank'>Вакцинировался</a> Sputnik V и всем советую (при отсутствии противопоказаний). Открыли вакансию в нашей команде — ищем <a href='https://hh.ru/vacancy/42225978'  target='_blank'>англоговорящего супер-методиста ⚡</a>. Стартовали прием заявок на <a href='https://hse.skoltech.ru' target='_blank'>совместную олимпиаду с Вышкой</a> по машинному обучению. Впервые провели курс Roadmap to Career Success для студентов Сколтеха в рамках <a href='https://www.skoltech.ru/en/education/about-skoltech-education/independent-studies-period/' target='_blank'>ISP</a>, и продолжаем делать вебинары с топовыми работодателями на <a href='http://career.skoltech.ru' target='_blank'>Online Career Hub</a>.";
+var modY1 = "#digest        upd 18/02/2021";
+var mod2 = "— <a href='https://youtu.be/89D-bWgVJME' target='_blank'>«منحة دراسية من جامعة سكولتيك للعلوم والتكنولوجيا» </a>, RT Online 2021 [ARA]<br> — <a href='https://vk.com/video-8711976_456239102' target='_blank'>«Roll Out с Денисом Столяровым»</a>, KPMG 2020 [RUS]<br> — <a href='https://youtu.be/imKW62S7ct8' target='_blank'>«Студенты на удаленке –  пионеры будущего?»</a>, EdCrunch OnDemand 2020 [RUS]<br> — <a href='https://youtu.be/lg0fuUNaERM' target='_blank'>«Breaking the Wall to Understanding Science»</a>, Falling Walls and World Science Summit 2020 [ENG]<br> — <a href='https://youtu.be/h6b457lA_Q4' target='_blank'>«DS, ML, AI — как англ. магистратура может помочь карьере»</a>, X5 Retail Hero 2020 [RUS]";
+var modY2 = "#presentations        the most recent five";
+var mod3 = "— <a href='http://sputniknews.cn/russia_china_relations/202007201031815681/' target='_blank'>报考斯科尔科沃科技学院中国考生人数翻倍</a>, Sputnik China 2020 [CHI]<br>— <a href='https://knife.media/stolyarov/' target='_blank'>«Как устроено образование в самом престижном техническом вузе России»</a>, НОЖ 2019 [RUS] <br>— <a href='https://postnauka.ru/author/d_stolyarov' target='_blank'>«Как выбрать университет и написать мотивационное письмо»</a>, ПостНаука 2019 [RUS] <br>— <a href='https://changellenge.com/article/check-list-magistratura/' target='_blank'>«Чек-лист: как не промахнуться при выборе магистратуры»</a>, Changellenge 2018 [RUS] <br>— <a href='https://vc.ru/flood/24633-stolyarov' target='_blank'> «Важный момент на собеседовании — это мотивация»</a>, VC 2017 [RUS]";
+var modY3 = "#publications         the most fav five";
+var mod4 = "Zero!";
+var modY4 = "#projects         👉🔥 to browse";
+var mod5 = "— <a href='https://www.facebook.com/stolyarovden/' target='_blank'>Facebook</a><br>— <a href='https://www.instagram.com/stolyarovden/' target='_blank'>Instagram</a><br>— <a href='https://twitter.com/stolyarovden' target='_blank'>Twitter</a><br>— <a href='https://vk.com/stolyarovden' target='_blank'>VKontakte</a><br>— <a href='https://www.linkedin.com/in/denis-stolyarov-8774a530' target='_blank'>LinkedIn</a><br><br> ... или пишите в Telegram по кнопке <a href='https://t.me/stolyarovden'><i class='fa fa-send'></i></a> внизу.";
+var modY5 = "#contacts        updated asynchronously";
+var mod6 = "Hi there! 👋 My name is Denis, I'm a Head of Student Department at the best young university in Russia – <a href='https://student.skoltech.ru' target='_blank'>Skoltech</a>, where I supervise students' outreach & recruitment activities, student services, alumni community programs development & career center. Most of the info on my website is in Russian, but some materials are not, so check out the language tag [ENG]. My contacts are listed <a href='#contact' onclick='pickMe(5)'>here</a>.";
+var modY6 = "#eng 🇬🇧         ←←← navigation hints";
+var sw = 0;
+var i;
+
+function projCall(){
+    var pr = "test2";
+    var r_text = new Array ();
+    
+    r_text[0] = "📖 books [RUS]<br><br><a href='https://eksmo.ru/book/eto-tochno-chyertova-dyuzhina-komiksov-o-nauke-i-uchyenykh-ITD925679/' target='_blank'> Это точно. Чёртова дюжина комиксов о науке и ученых </a> — первый в России сборник sci-комиксов, каждый из которых основан на реальном научном исследовании // длинный список <a href=' http://premiaprosvetitel.ru/news/view/?362' target='_blank'>премии «Просветитель» 2019</a><br>";
+    r_text[1] = "📖 books [RUS]<br><br><a href='https://www.litres.ru/zahar-prilepin/kak-my-perezhili-voynu-narodnye-istorii/' target='_blank'> Как мы пережили войну. Народные истории </a> — Галина Юзефович и еще более 100 авторов о своих семьях в Великую отечественную войну // с. 209-213 — история Столяровой Надежды Терентьевны<br>";
+    r_text[2] = "🎫 events [ENG]<br><br><a href='https://youtu.be/3LIMXvRQ-Eg' target='_blank'> Skoltech Online Graduation 2020 </a> — первый онлайн-выпускной в истории Сколтеха // ведущая — невероятная Таня Геворкян<br>";
+    r_text[3] = "🎫 events [ENG]<br><br><a href='https://student.skoltech.ru/falling-walls-lab' target='_blank'> Falling Walls Lab Moscow </a> —  уникальная возможность рассказать о своем проекте всему миру всего за три минуты // финалисты получают приглашение на <a href='https://berlinscienceweek.com' target='_blank'>Berlin Science Week</a><br>";
+    r_text[4] = "💻 media [ENG]<br><br><a href='https://youtu.be/kY5fAJxPVyk' target='_blank'> Skoltech 365 </a> — один год из жизни студента Сколтеха в двух минутах шестнадцати секундах  // самое популярное видео на канале Сколтеха<br>";
+    r_text[5] = "💻 media [RUS]<br><br><a href='https://postnauka.ru/specials/careersinscience' target='_blank'> Как поступить </a> —  карьерный путеводитель по науке и высоким технологиям для студентов и молодых ученых, созданный вместе с ПостНаукой // ценные советы и практические гайды от профессоров с мировым именем и молодых ученых Сколтеха";
+    
+    if(sw == 0){
+        i = Math.floor(r_text.length * Math.random());
+        pr = r_text[i];  
+        sw = 1;
+    }else{
+        if(r_text.length <= i){
+            i = 0;
+        }
+        pr = r_text[i];
+    }
+    i = i + 1;
+    return pr;
+}
+
+function pickMe(x) {
+  var y = document.getElementById("myDIV");
+  var y1 = document.getElementById("tag");
+  var y2 = "test3";
+
+  if (x === 1) {
+    y.innerHTML = mod1;
+    y1.innerHTML = modY1;      
+  }
+  else if (x === 2) {
+    y.innerHTML = mod2;
+    y1.innerHTML = modY2;
+  }
+  else if (x === 3) {
+    y.innerHTML = mod3;
+    y1.innerHTML = modY3;
+  }
+  else if (x === 4) {
+    y2 = projCall();
+    y.innerHTML = y2;
+    y1.innerHTML = modY4;
+  }
+  else if (x === 5) {
+    y.innerHTML = mod5;
+    y1.innerHTML = modY5;
+  }
+  else if (x === 6) {
+    y.innerHTML = mod6;
+    y1.innerHTML = modY6;
+  }
+  else if (x === 0) {
+    y.innerHTML = mod0;
+  }
+}
+
+function timestamp(){
+    var today = new Date();
+    var time = ("0" + today.getHours()).slice(-2) + ":" + ("0" + today.getMinutes()).slice(-2);
+    document.getElementById('tag').innerHTML = time + " " +"<i class='ti-double-check'></i>";
+}
